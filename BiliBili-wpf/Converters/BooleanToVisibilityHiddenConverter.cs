8dafa5627e5a55908815a1053f0822e3e3732c94
tangchen <1601865659@qq.com>
2025-03-24 00:09:05 +0800

    哔哩哔哩，导航

---
+++ BiliBili-wpf/Converters/BooleanToVisibilityHiddenConverter.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace BiliBili_wpf.Converters;
+
+public class BooleanToVisibilityHiddenConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is bool boolValue)
+        {
+            return boolValue ? Visibility.Visible : Visibility.Hidden;
+        }
+        return Visibility.Hidden; // 默认返回 Hidden
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is Visibility visibilityValue)
+        {
+            return visibilityValue == Visibility.Visible;
+        }
+        return false;
+    }
+}
